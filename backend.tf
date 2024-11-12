baeca41ed8ef1b382e3e4a3206005fe6bd4b20a9
Omer Zubair <omerzubair.ae@gmail.com>
2024-11-11 22:21:01 -0500

    3rd commit

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-information-btc"
+    bucket         = "terraform-state-information-btc11"
     region         = "ca-central-1"
     key            = "state/terraform.tfstate"
     // dynamodb_table = "dynamodb-state-information"
